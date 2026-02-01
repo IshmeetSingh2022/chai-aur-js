1ae2fe33025d261f06bc91a87d914f286c16eb1e
IshmeetSingh2022 <145209087+IshmeetSingh2022@users.noreply.github.com>
2026-02-01 18:03:53 +0000

    promise basics

---
+++ 08_basics/promise01.js
@@ -0,0 +1,63 @@
+// new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log('Async task is completed')
+//         resolve()
+//     },1000)
+    
+// }).then(()=>{
+//     console.log('Promise resolved')
+// })
+
+// const promise2=new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve({username:'Ishmeet',age:21})
+//     }, 1000);
+// })
+
+// promise2.then((obj)=>{
+//     console.log(obj)
+// })
+
+
+const Promise3=new Promise((resolve,reject)=>{
+    let error=true
+    if(!error){
+         resolve({username:'kavan',age:22})
+    }
+    else{
+        reject('Something went wrong')
+    }
+})
+
+Promise3.then((user)=>{
+return user.username
+}).then((username)=>{
+    console.log(username)
+}).catch((error)=>{
+console.log(error) 
+}).finally(()=>{
+    console.log('the promise is either resolved or rejected')
+})
+
+
+const Promise4=new Promise((resolve,reject)=>{
+    let error=true
+    if(!error){
+         resolve({username:'kavaasdadn',age:22})
+    }
+    else{
+        reject('Js went wrong')
+    }
+})
+
+async function fetchPromise4(){
+    try{
+    const resp=await Promise4
+    console.log(resp)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+fetchPromise4()
